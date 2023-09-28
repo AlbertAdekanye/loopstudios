6964044d57878f001b029ab53507bff260873a11
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-09-28 18:12:13 +0100

    added custom screen sizes, colors and fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+    extend: {
+      colors: {
+        darkGray: 'hsl(0, 0%, 55%)', // Add a comma here
+        veryDarkGray: 'hsl(0, 0%, 41%)', // Add a comma here
+      },
+      fontFamily: {
+        body: ['"Poppins"', 'sans-serif', 'Josefin Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
